a54b6285b88f840f149c08ed3cbd0c962bc0d98c
siva krishna <sivakrishna053936@gmail.com>
2024-09-03 10:59:24 +0530

    test

---
+++ JAVA/JAVA8_features/Comparator/Myclass.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Myclass implements Comparator<Integer> {           // this class is used in the Test class to sort out the enames
+    public int compare(Integer a, Integer b){
+            return b-a;
+    }
+    
+}
--- JAVA/JAVA8_features/Comparator/Test.java
+++ JAVA/JAVA8_features/Comparator/Test.java
@@ -2,13 +2,16 @@ import java.util.*;
 
 public class Test{
     public static void main(String[] args) {
-        List<Integer> enames=new ArrayList<>();
-        enames.add(56);
-        enames.add(789);
-        enames.add(45);
-        enames.add(265);
-        enames.add(321);
-        Collections.sort(enames,(a,b)->b-a);          // a-b means a Ascending order and b-a means descending order
-        System.out.println(enames);
+        List<Integer> eids=new ArrayList<>();
+        eids.add(56);
+        eids.add(789);
+        eids.add(45);
+        eids.add(265);
+        eids.add(321);
+
+        // below line no 13 is by using the lambda expression without creating a extra Myclass for implementing the Comparator interface
+       // Collections.sort(eids,(a,b)->b-a);          // a-b means a Ascending order and b-a means descending order
+       Collections.sort(eids,new Myclass());
+        System.out.println(eids);
     }
 }
