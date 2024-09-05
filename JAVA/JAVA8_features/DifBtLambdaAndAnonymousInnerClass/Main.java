5335f5768e6fbee72cffe1a356e7754b2ac401ed
siva krishna <sivakrishna053936@gmail.com>
2024-09-05 15:37:38 +0530

    test

---
+++ JAVA/JAVA8_features/DifBtLambdaAndAnonymousInnerClass/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // here in the commment lines lines represents the ANONYMOUS INNER CLASS PART
+        
+        // One o1=new One (){
+        //    public  int  getSalary(){
+        //         return 5000;
+        //     }
+        //     public String empName(){
+        //             return "Sivakrishna";
+        //     }
+        // };
+
+        // System.out.println(o1.getSalary());
+        // System.out.println(o1.empName());
+
+
+
+        // Here We use the Lambda expresssion but this lamBAd expression is Applicable only when it is a Functional Interface
+      One o1=()->{
+      return "Sivakrishna";
+      };
+    //   for(int i=1;i<=10;i++){
+    //     System.out.println("Krishna" + i);
+    // }
+
+    }
+}
---
+++ JAVA/JAVA8_features/DifBtLambdaAndAnonymousInnerClass/One.java
@@ -0,0 +1,9 @@
+import java.util.*;
+/**
+ * One
+ */
+public interface One {
+            public String empName();
+            // public int getSalary();
+    
+}
