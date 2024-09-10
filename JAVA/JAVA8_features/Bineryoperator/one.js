eac5a1262debaec92f1ebe49dc3885c9143941ae
siva krishna <sivakrishna053936@gmail.com>
2024-09-10 13:07:54 +0530

    test

---
+++ JAVA/JAVA8_features/Bineryoperator/one.js
@@ -0,0 +1,10 @@
+let numbers=[];
+
+let task=()=>{
+    for(i=1;i<10;i+3){
+        switch(){
+
+        }
+    }
+}
+
---
+++ JAVA/JAVA8_features/Method_refANDConstructor/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Test
+ */
+public class Test {
+
+    public void ename(String name){
+            System.out.println(name);
+    }
+    public static void main(String[] args) {
+        Test test=new Test();
+        
+        // System.out.println(test::ename);
+    }
+}
