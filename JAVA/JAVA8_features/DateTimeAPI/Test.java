9c81462431f8b0a3e2d91a5c4c0fac4b0a457d40
siva krishna <sivakrishna053936@gmail.com>
2024-09-11 11:15:12 +0530

    test

---
+++ JAVA/JAVA8_features/DateTimeAPI/Test.java
@@ -0,0 +1,9 @@
+import java.time.LocalTime;
+import java.util.*;
+
+public class Test{
+    public static void main(String[] args) {
+      LocalTime now= LocalTime.now();
+       System.out.println(now);
+    }
+}
